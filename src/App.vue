acfb7cb10d5da8257847317d20505cf91b70486c
Arsen Merkin <64312315+a-merkin@users.noreply.github.com>
2023-04-26 15:05:08 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    style="
+      display: flex;
+      width: 100%;
+      height: 100vh;
+      justify-content: center;
+      align-items: center;
+    "
+  >
+    <div style="height: 900px; width: 1600px">
+      <l-map
+        :options="{ attributionControl: false }"
+        :useGlobalLeaflet="false"
+        ref="map"
+        v-model:zoom="zoom"
+        :center="[47.41322, -1.219482]"
+      >
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        ></l-tile-layer>
+      </l-map>
+    </div>
+  </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 2,
+    };
+  },
+};
+</script>
+
+<style></style>
+
